0b1f6dfdf460b031f187bbe0d6752c531ba7f994
Sagar Jha <sagarjha0511@gmail.com>
2023-08-10 17:07:39 +0530

    center

---
+++ object.js
@@ -0,0 +1,14 @@
+//object 
+let person = {
+    name: "john"
+    age: 44;
+    grade:  ["A","B","C"];
+    greet: function(){
+        console.log(this.name)
+    }
+    
+}
+name: "john"
+    age= 44;
+    grade= ["A","B","C"];
+    function()
